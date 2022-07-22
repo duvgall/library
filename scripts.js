6303b0863bb8dff8fc3651f03fbd6e0c2ab13e95
Alan Doyle <alanduvgall@gmail.com>
2022-07-22 07:58:49 +0100

    add functions to accept form input and push to array

--- scripts.js
+++ scripts.js
@@ -4,22 +4,57 @@ const addBtn = document.getElementById('add-book');
 
 newBtn.addEventListener('click', () => form.style.display = 'block');
 
-addBtn.addEventListener('click', addBookToLibrary);
+addBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let finished = document.getElementById('finished');
 
-class Book {
-    constructor(title, author, pages, finished) {
+    if (finished.checked === true) {
+        finished.value = 'read';
+    } else {
+        finished.value = 'not read';
+    }; 
+    finished = finished.value;
+    const newBook = new Book(title, author, pages, finished)
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    title = '';
+    author = '';
+    pages = '';
+    form.style.display = 'none';
+    resetForm();
+});
+
+function Book(title, author, pages, finished) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.finished = finished;
-    }
-}
+    
+};
 
 let myLibrary = []
-let newBook;
 
-function addBookToLibrary() {
+/*function addBookToLibrary() {
     let newBook = new Book(title, author, pages, finished);
     myLibrary.push(newBook);
     console.log(myLibrary);
+}*/
+
+/*function addBook() {
+   
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let finished = document.getElementById('finished').value;
+
+    const newBook = new Book(title, author, pages, finished)
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+}*/
+
+function resetForm() {
+    form.reset();
 }
